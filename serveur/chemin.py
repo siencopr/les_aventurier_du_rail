48589f9e97c48946789fa345e24aea5113799d87
llois <llois.jourdan@gmail.com>
2023-05-22 19:26:52 +0200

    tien

--- serveur/chemin.py
+++ serveur/chemin.py
@@ -1,11 +1,12 @@
 from constantes import *
 class Chemin:
-    def __int__(self, couleur, taille, numero, type):
+    def __init__(self, couleur, taille, numero, type, villeA, villeB):
         self.couleur = couleur
         self.taille = taille
         self.numero = numero
         self.type = type
         self.joueur_detenteur = None
         self.point = DICO_EQUIVALANCE_LONGUEUR_POINTS[self.taille]
+        self.relie = (villeA, villeB)
     def __repr__(self):
         return self.numero
--- serveur/main.py
+++ serveur/main.py
@@ -4,3 +4,4 @@ from joueur import Joueur
 j1 = Joueur('j1', ['1','2','3','4'])
 print('nom', j1.nom)
 print('destination', j1.carte_destination)
+
