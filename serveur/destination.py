ea0fedbd985ac88063ae11a3034560ce07d153a0
Mey <meynever@gmail.com>
2023-05-22 20:00:53 +0200

    class carte dest

--- serveur/destination.py
+++ serveur/destination.py
@@ -1,19 +1,19 @@
 from constantes import *
 
-class destination:
-    def __int__(self,ville1,ville2,points):
+class Destination:
+    def __int__(self,ville_1,ville_2,points):
         self.ville1 = None
         self.ville2 = None
         self.points = 0
 
     def __repr__(self):
-        return str(self.ville1) + "  <=>  " + str(self.ville2) +"  :  "+str(self.points)
+        return str(self.ville1) + "  <=>  " + str(self.ville2) + "  :  " +str(self.points)
 
 destinationslongues = [
-destination(ville.CedarRapids,ville.Rouyn,19)
+Destination(ville.CedarRapids,ville.Rouyn,19)
 
 
 
 
 ]
-print(destinations_longues)
+print(destinationslongues)
